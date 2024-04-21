99d84a2d00edce308c96e30c387596afca04c00a
zhiyuan feng <tiger.zhiyuanfeng@gmail.com>
2024-04-20 22:23:10 -0600

    remove test view

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HomeView, AboutView, DashboardView, ProfileView, EventView, FansView
-from .views import test
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
