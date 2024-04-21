b1572251f2f44ed5ce0a6e9c39861baeb9ab1626
zhiyuan feng <tiger.zhiyuanfeng@gmail.com>
2024-04-20 22:20:28 -0600

    modified profile page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import View, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
@@ -16,6 +16,27 @@ class DashboardView(LoginRequiredMixin,TemplateView):
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/profile.html'
+
+    def _update_name(self, user, first_name, last_name):
+        updated = False
+        if first_name:
+            user.first_name = first_name
+            updated = True
+        if last_name:
+            user.last_name = last_name
+            updated = True
+        
+        if updated:
+            user.save()
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        self._update_name(user, first_name, last_name)
+
+        return redirect('profile')
+
 
 class EventView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/event.html'
