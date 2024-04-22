7d1f8a8e75c1af2d134d4983f68259d2d12c7f22
zhiyuan feng <tiger.zhiyuanfeng@gmail.com>
2024-04-22 10:36:05 -0600

    url config bug and optimize pages tag

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import EventView, CreateEventView, EventDetailView
 
 urlpatterns = [
-    path('event/', EventView.as_view(), name='event'),
+    path('', EventView.as_view(), name='event'),
     path('create/', CreateEventView.as_view(), name='create-event'),
-    path('events/<pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('detail/<pk>/', EventDetailView.as_view(), name='event-detail'),
 ]
